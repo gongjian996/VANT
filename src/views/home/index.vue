a09e5eefb5269d5854d7db4de9718992097e534b
lipengzhou <lpzmail@163.com>
2019-07-14 11:28:18 +0800

    拉黑作者-完成

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -115,7 +115,7 @@
       <van-cell-group v-if="!toggleRubbish">
         <van-cell title="不感兴趣" @click="handleDislick" />
         <van-cell title="反馈垃圾内容" is-link @click="toggleRubbish = true" />
-        <van-cell title="反馈垃圾内容" />
+        <van-cell title="拉黑作者" @click="handleAddBlacklist" />
       </van-cell-group>
       <van-cell-group v-else>
         <van-cell icon="arrow-left" @click="toggleRubbish = false" />
@@ -132,6 +132,7 @@
 <script>
 import { getUserChannels } from '@/api/channel'
 import { getArticles, dislikeArticle } from '@/api/article'
+import { addBlacklist } from '@/api/user'
 import HomeChannel from './components/channel'
 
 export default {
@@ -367,6 +368,14 @@ export default {
 
       // 把本条数据移除
       articles.splice(delIndex, 1)
+
+      this.$toast('操作成功')
+    },
+
+    async handleAddBlacklist () {
+      await addBlacklist(this.currentArticle.aut_id)
+      this.isMoreActionShow = false
+      this.$toast('操作成功')
     }
   }
 }
