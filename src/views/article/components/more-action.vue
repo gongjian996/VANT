8124bd229a3edab4ec49457333a2aa14eaa98596
lipengzhou <lpzmail@163.com>
2019-07-17 09:09:05 +0800

    文章详情-组件组织

---
+++ src/views/article/components/more-action.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>更多操作</div>
+</template>
+
+<script>
+export default {
+  name: 'MoreAction',
+  props: {},
+  data () {},
+  created () {},
+  methods: {}
+}
+</script>
+
+<style lang="less" scoped></style>
---
+++ src/views/article/components/recommend-article.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>推荐文章</div>
+</template>
+
+<script>
+export default {
+  name: 'RecommendArticle',
+  props: {},
+  data () {},
+  created () {},
+  methods: {}
+}
+</script>
+
+<style lang="less" scoped></style>
